3c7f3bb1514e3f5e3609aedbe9c328732f8e3448
Peter Burkholder <pburkholder@chef.io>
2016-02-12 09:56:56 -0500

    Initial ugly attempt

---
+++ attributes/default.rb
@@ -0,0 +1,1 @@
+default['vault-template']['sum'] = '356ba9c1c02202ae128899bbb0b7668aa527df461d4077d1a5331d77d632a8a4'
--- recipes/default.rb
+++ recipes/default.rb
@@ -11,18 +11,27 @@ aws = lambda do
   chef_vault_item('credentials', 'aws')
 end
 
-new_sha = lambda do
-  Chef::Log.warn('in new_sha')
-  true
-end
 
-template '/etc/s3cfg' do
-  source 's3cfg.erb'
-  owner 'root'
-  group 'root'
-  mode 00744
-  variables lazy {
-    { aws_access_key: aws.call['aws_access_key'] }
-  }
-  only_if {new_sha.call()}
+# this works if the secret changes
+b = Chef::DataBagItem.load('credentials', 'aws')
+Chef::Log.warn(b.to_json)
+encrypted_db_sum = Digest::SHA256.hexdigest(b.to_json)
+Chef::Log.warn(encrypted_db_sum)
+
+if node['vault-template']['sum'] != encrypted_db_sum then
+  Chef::Log.warn("Running refresh code")
+
+# but not if '/etc/s3cfg' is gone
+  template '/etc/s3cfg' do
+    source 's3cfg.erb'
+    owner 'root'
+    group 'root'
+    mode 00744
+    variables lazy {
+      { aws_access_key: aws.call['aws_access_key'] }
+    }
+  end
+
+  node.default['vault-template']['sum'] = encrypted_db_sum
+
 end
